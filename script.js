5d4746429c2d9c01355db6f476b18847e8e543be
aneyeopen <daniel.martin603@icloud.com>
2022-09-23 21:57:15 +1000

    added drop down menu for color change, only works after clearing. normal buttons would have worked better but i just wanted to try using the drop-down

--- script.js
+++ script.js
@@ -3,19 +3,49 @@ const etchContainer = document.getElementById("container");
 let gridRow = 16;
 let gridCol = 16;
 
-function makeGrid(rows, cols) {
+const ClearButton = document.getElementById('clearButton');
+
+let activeColor =  document.getElementById("color").value;
+
+function colorCheck(activeColor) {
+    let selectedColor = document.getElementById("color").value;
+    console.log(selectedColor)
+    if (selectedColor = 'Red') {
+        activeColor = 'Red';
+    }else {
+        activeColor = "Black";
+    } return activeColor;
+}
+
+function makeGrid(rows, cols, activeColor) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
+    colorCheck(activeColor);
+    let color = activeColor;
+    console.log(color);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
         cell.innerText = " ";
         container.appendChild(cell).className = "gridItem unPainted";
         cell.addEventListener('mouseover', 
-        e => e.target.classList.add('painted'))
+        e => e.target.classList.add(color))
     };
   };
 
-  makeGrid(gridRow, gridCol);
+
+  function reloadGrid() {
+    clearGrid();
+    makeGrid(gridRow, gridCol, activeColor);
+  }
+  
+  function clearGrid() {
+    etchContainer.innerHTML = ''
+  }
+
+  makeGrid(gridRow, gridCol, activeColor);
+
+  clearButton.addEventListener('click', (event) => {reloadGrid()});
+
 
   
 
